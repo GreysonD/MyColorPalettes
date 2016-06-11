19dbc20a54226cc73171d431c714e535490ec457
Greyson Dehn <greyson.dehn@gmail.com>
2016-06-11 15:59:27 -0500

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -2,52 +2,56 @@ function ColorListCtrl($scope) {
   $scope.colors = [
   	{
   		"title": "BOULDER",
-  		"data": [{
-    			"name": "light",
-    			"code": "#2384c6"
+  		"data": [
+  			{
+    			"name": "dark",
+    			"code": "#1a4278"
   			},
   			{
     			"name": "regular",
     			"code": "#045eaa"
   			},
   			{
-    			"name": "dark",
-    			"code": "#1a4278"
+    			"name": "light",
+    			"code": "#2384c6"
   			}
   		],
   		"tags": []
   	},
   	{
   		"title": "YOGA",
-  		"data": [{
-    			"name": "light",
-    			"code": "#faa21c"
+  		"data": [
+  		  {
+    			"name": "dark",
+    			"code": "#e25a20"
   			},
   			{
     			"name": "regular",
     			"code": "#f57921"
   			},
   			{
-    			"name": "dark",
-    			"code": "#e25a20"
+    			"name": "light",
+    			"code": "#faa21c"
   			}
   		],
   		"tags": []
   	},
   	{
   		"title": "FITNESS",
-  		"data": [{
-    			"name": "light",
-    			"code": "#9ac93d"
+  		"data": [
+  		  {
+    			"name": "dark",
+    			"code": "#509634"
   			},
   			{
     			"name": "regular",
     			"code": "#7eb138"
   			},
   			{
-    			"name": "dark",
-    			"code": "#509634"
+    			"name": "light",
+    			"code": "#9ac93d"
   			}
+  			
   		],
   		"tags": []
   	},
